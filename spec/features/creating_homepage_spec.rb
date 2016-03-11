57626aeecc4bcac6d19f15bfbf493f040ad472e5
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-03-11 08:03:23 -0500

    Added Homepage

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature 'Creating Home Page' do
+  
+  scenario do
+    visit root_path
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_link("Home")
+    expect(page).to have_content("Workout Lounge")
+  end
+
+end
