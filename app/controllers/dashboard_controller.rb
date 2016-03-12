d05650955cc095274c332d3cf1776e558828f55b
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-03-12 11:30:43 -0500

    Added search feature

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
+  
   def index
     @athletes = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def search
+    @athletes = User.search_by_name(params[:search_name]).paginate(page: params[:page], per_page: 10)
   end
 end
