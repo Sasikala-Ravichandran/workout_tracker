c8f278baf3df51d96268e05057db496073deae20
Sasikala-Ravichandran <passion.rails@gmail.com>
2016-03-12 05:01:15 -0500

    Added user firstname lastname

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -6,11 +6,14 @@ RSpec.feature 'Signing up user' do
 
     visit root_path
     click_link "Sign up"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with: "johnd@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button 'Sign up'
     expect(page).to have_content("You have signed up successfully.")
-  
+    visit "/"
+    expect(page).to have_content("John Doe")
   end
 end
